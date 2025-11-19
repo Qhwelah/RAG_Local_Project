678b7a23c692448e15ad026021250194357d7b35
qhwelah <jdcooper814@gmail.com>
2025-11-19 14:51:47 -0500

    added python exception handler

--- rag-cli/app.py
+++ rag-cli/app.py
@@ -10,32 +10,39 @@ PGPASSWORD = os.getenv("PGPASSWORD")
 
 print(f"Variables are: PGUSER:{PGUSER}, PGPASSWORD:{PGPASSWORD}")
 
-# Connect to postgres service
-conn = psycopg.connect(f"host=pg dbname=rag user={PGUSER} password={PGPASSWORD}")
-with conn, conn.cursor() as cur:
-    cur.execute("""
-    Create extension vector;
-    """)
-    
-    time.sleep(2)
 
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS chunks(
-        id bigserial PRIMARY KEY,
-        doc_id text,
-        chunk_index int,
-        text text,
-        embedding vector(768)
-    );
-    """)
+try:
+    # Connect to postgres service
+    conn = psycopg.connect(f"host=pg dbname=rag user={PGUSER} password={PGPASSWORD}")
+    with conn, conn.cursor() as cur:
+        cur.execute("""
+        Create extension vector;
+        """)
+        
+        time.sleep(2)
+
+        cur.execute("""
+        CREATE TABLE IF NOT EXISTS chunks(
+            id bigserial PRIMARY KEY,
+            doc_id text,
+            chunk_index int,
+            text text,
+            embedding vector(768)
+        );
+        """)
 
 
-print("Imports done, we're in the script lol")
+    print("Imports done, we're in the script lol")
 
-print("Can we make a numpy array?")
-np_zeroes_test = np.zeroes((3, 4))
-print(np_zeroes_test)
-print("The answer is yes I think maybe, see above")
+    print("Can we make a numpy array?")
+    np_zeroes_test = np.zeros((3, 4))
+    print(np_zeroes_test)
+    print("The answer is yes I think maybe, see above")
+
+
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+
 
 while time.sleep(30):
     print("sleeping...")
