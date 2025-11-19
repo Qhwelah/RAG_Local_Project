84a4743f43da4c12415d098dfdd774d0de5cbb2f
qhwelah <jdcooper814@gmail.com>
2025-11-19 17:50:03 -0500

    add logs, keep container running

--- rag-cli/app.py
+++ rag-cli/app.py
@@ -4,23 +4,25 @@ from sentence_transformers import SentenceTransformer
 import requests, sys
 import os, time
 
-# Get environment variables
-PGUSER = os.getenv("PGUSER")
-PGPASSWORD = os.getenv("PGPASSWORD")
-
-print(f"Variables are: PGUSER:{PGUSER}, PGPASSWORD:{PGPASSWORD}")
-
-
 try:
+    # Get environment variables
+    PGUSER = os.getenv("PGUSER")
+    PGPASSWORD = os.getenv("PGPASSWORD")
+    print(f"Variables are: PGUSER:{PGUSER}, PGPASSWORD:{PGPASSWORD}")
+    
+
     # Connect to postgres service
+    print("Initializing postgres vector database...")
     conn = psycopg.connect(f"host=pg dbname=rag user={PGUSER} password={PGPASSWORD}")
     with conn, conn.cursor() as cur:
+        print("Enabling vector extension...")
         cur.execute("""
-        Create extension vector;
+        CREATE EXTENSION IF NOT EXISTS vector;
         """)
         
         time.sleep(2)
 
+        print("Creating chunks table...")
         cur.execute("""
         CREATE TABLE IF NOT EXISTS chunks(
             id bigserial PRIMARY KEY,
@@ -30,19 +32,22 @@ try:
             embedding vector(768)
         );
         """)
+    print("Postgres vector database initialized successfully.")
 
 
-    print("Imports done, we're in the script lol")
-
+    # Imports test
     print("Can we make a numpy array?")
-    np_zeroes_test = np.zeros((3, 4))
-    print(np_zeroes_test)
+    np_zeros_test = np.zeros((3, 4))
+    print(np_zeros_test)
     print("The answer is yes I think maybe, see above")
 
 
+# Handle exceptions
 except Exception as e:
     print(f"An unexpected error occurred: {e}")
 
 
-while time.sleep(30):
+# Keep container running (Docker container exists otherwise)
+while True:
     print("sleeping...")
+    time.sleep(30)
