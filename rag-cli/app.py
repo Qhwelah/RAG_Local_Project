cf7af8232c11af5dd116f3828f8189ba66f9ec1f
qhwelah <jdcooper814@gmail.com>
2025-11-21 16:11:57 -0500

    laid out plan for ingestion, vector search, and LLM calls

--- rag-cli/app.py
+++ rag-cli/app.py
@@ -4,6 +4,14 @@ from sentence_transformers import SentenceTransformer
 import requests, sys, logging
 import os, time
 
+## ========== Parameter settings ========== ##
+SENTENCE_TRANSFORMER_MODEL = "BAAI/bge-small-en-v1.5"
+EMBEDDING_VECTOR_DIMENSIONS = 384   #for BAAI/bge-small-en-v1.5
+
+# SENTENCE_TRANSFORMER_MODEL = "BAAI/bge-small-en-v1.5"
+# EMBEDDING_VECTOR_DIMENSIONS = 768   #for BAAI/bge-base-en-v1.5
+## ======================================== ##
+
 # Configure logger
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -27,16 +35,52 @@ try:
         time.sleep(2)
 
         logger.info("Initializing chunks table...")
+        # Make sure the vector dimensions number matches that of the model you are calling
         cur.execute("""
         CREATE TABLE IF NOT EXISTS chunks(
             id bigserial PRIMARY KEY,
             doc_id text,
             chunk_index int,
             text text,
-            embedding vector(768)
+            embedding vector(%s)
         );
-        """)
+        """, (EMBEDDING_VECTOR_DIMENSIONS))
     logger.info("Postgres vector database initialized successfully.")
+
+    # Initial file ingestion and context database filling
+        # Chunking
+            ## Pure hard character limit, or using tiktoken
+
+        # Embedding
+            ## Sentence Transformer (use model SENTENCE_TRANSFORMER_MODEL)
+
+
+    # Connect to ollama
+        ## r = requests.post("http://ollama:11434/api/generate", json={"model": model, "prompt": prompt, "stream": False})
+
+    # LLM call loop
+        # Wait for user input
+            ## External text editor perhaps, or multiline text entry
+            ## See `user_prompt_input.py`
+
+        # Chunk and vectorize the user's input
+            ## Same chunking and embedding functions from above
+
+        # Call psql vector distance comparison
+            ## Using <-> operator to find vector distances
+            ## "Retrieval"
+
+        # With the k closest results of the vector search prepended, call the LLM model
+            ## Prompt: "Use the context to answer.\n\nContext:\n{ctx}\n\nQuestion: {q}\nAnswer:"
+            ## "Augmented Generation"
+        
+        # Print out the model's response to the user
+            ## answer = ollama_generate(prompt)
+            ## print(answer)
+        
+        # Repeat  
+
+
 
 
 # Handle exceptions
@@ -45,6 +89,6 @@ except Exception as e:
 
 
 # Keep container running (Docker container exists otherwise)
-logger.debug("sleeping...")
+logger.info("sleeping...")
 while True:
     time.sleep(120)
