b1a3948da10190024da6e93e4d78b8d478dcaf0b
qhwelah <jdcooper814@gmail.com>
2025-12-10 22:27:44 -0500

    test more picky soup search

--- rag-cli/scraper.py
+++ rag-cli/scraper.py
@@ -4,16 +4,45 @@ from urllib.parse import urljoin
 from collections import deque
 import json
 
+
 BASE = "https://harrisburg.psu.edu/counseling-psychological-services"
 
+
 def is_in_desired_subtree(url: str) -> str:
-    url = url.split('#', 1)[0].split('?', 1)[0]
     return url.startswith(BASE)
+
+
+def get_canonical_url(url: str) -> str:
+    return url.split('#', 1)[0].split('?', 1)[0]
+
 
 def extract_text(html: str) -> str:
     soup = BeautifulSoup(html, "html.parser")
-    main = soup.find(id="block-desired-content") or soup.body
-    return main.get_text(separator="\n", strip=True) if main else ""
+
+    # Remove all non-content sections of the page
+    for tag in soup(["header", "nav", "footer", "form", "script", "style", "noscript"]):
+        tag.decompose()
+
+    # Search for only 'main' content of page. If cannot find, just use the html 'body' tag
+    main = (
+        soup.find("main") or
+        soup.find(attrs={"role": "main"}) or
+        soup.body
+    )
+
+    if not main:
+        return ""
+
+    text = main.get_text(separator="\n", strip=True) 
+
+    lines = [line.strip() for line in text.splitlines() if line.strip()]
+    return "\n".join(lines)
+
+
+    # main = soup.find(id="block-desired-content") or soup.body
+    # return main.get_text(separator="\n", strip=True) if main else ""
+
+
 
 def crawl_desired_tree():
     visited = set()
@@ -22,6 +51,9 @@ def crawl_desired_tree():
 
     while queue:
         url = queue.popleft()
+        url = get_canonical_url(url)
+
+        # if site already visited, ignore it
         if url in visited:
             continue
         visited.add(url)
@@ -36,7 +68,9 @@ def crawl_desired_tree():
 
             text = extract_text(resp.text)
             results.append({"url": url, "text": text})
+            #print(f"Text from this page is: {text}\n")
 
+            # Find all the linked urls in this page and push them to the queue
             soup = BeautifulSoup(resp.text, "html.parser")
             for a in soup.find_all("a", href=True):
                 next_url = urljoin(url, a["href"])
