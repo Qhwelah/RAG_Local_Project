da96783311a9388932f2cee136d20950465c6c0c
qhwelah <jdcooper814@gmail.com>
2025-12-14 15:15:20 -0500

    working (messy) embedder

--- rag-cli/ingestion.py
+++ rag-cli/ingestion.py
@@ -1,10 +1,14 @@
 import json
 import logging
 import text_chunker
+from sentence_transformers import SentenceTransformer
+
+SENTENCE_TRANSFORMER_MODEL = "BAAI/bge-base-en-v1.5"
 
 logger = logging.getLogger(__name__)
 
 def ingest_data():
+    # Pull data from cached data (from last scrape)
     data = []
     try:
         with open("/data/web_pages.jsonl", 'r') as file:
@@ -20,6 +24,7 @@ def ingest_data():
     logger.debug(f"Input data to chunk: {data}")
 
 
+    # Chunking
     logger.info("Beginning chunking... ")
     chunked_documents = []
     try:
@@ -42,9 +47,31 @@ def ingest_data():
         logger.error(f"Error during chunking: {type(e)} - {e}")
         raise(e)
     
+    # chunk_num = 0
+    # for chunk in chunked_documents:
+    #     logger.debug(f"Chunk {chunk_num}: {chunk}")
+    #     chunk_num += 1
+
     logger.info("Chunking completed successfully!")
-    
-    chunk_num = 1
-    for chunk in chunked_documents:
-        logger.debug(f"Chunk {chunk_num}: {chunk}")
-        chunk_num += 1
+
+
+    # Embedding
+    logger.info("Beginning embedding...")
+    embedding_model = SentenceTransformer(SENTENCE_TRANSFORMER_MODEL)
+    embedded_chunks = []
+    chunk_index = 0
+
+    try:
+        for chunk in chunked_documents:
+            ch_embedding = embedding_model.encode(chunk['text'])
+            logger.debug(f"Chunk {chunk_index}: {chunk}\nEmbedding: {ch_embedding}\n")
+            chunked_documents[chunk_index]['embedding'] = ch_embedding
+
+            if(chunk_index < 3):
+                logger.debug(f"Final chunk entry: {chunked_documents[chunk_index]}\n")
+
+            chunk_index += 1
+            
+    except Exception as e:
+        logger.error(f"Error during embedding chunk index #{chunk_index}: {type(e)} - {e}")
+        raise(e)
