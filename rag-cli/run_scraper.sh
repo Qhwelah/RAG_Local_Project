be41c4561f45963845453d912232d86259c3f63a
qhwelah <jdcooper814@gmail.com>
2025-12-13 22:12:56 -0500

    universalized scraping

--- rag-cli/run_scraper.sh
+++ rag-cli/run_scraper.sh
@@ -1,6 +1,6 @@
-cd /universal
+cd /universal_crawler
 echo "Running website crawl..."
-scrapy crawl universal --loglevel=INFO -a base="https://harrisburg.psu.edu/counseling-psychological-services" -a subtree=1 -O "web_pages.jsonl" 
+scrapy crawl universal --loglevel=INFO -a base=${SCRAPE_URL} -a subtree=1 -O "web_pages.jsonl" 
 
 # Copy the results into the persisting docker volume
 cp web_pages.jsonl /data/web_pages.jsonl
