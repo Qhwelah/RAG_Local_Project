9780bbb0d0511e1cc77e7bdac0ca32d466a9b213
qhwelah <jdcooper814@gmail.com>
2025-12-13 22:34:09 -0500

    variable sharing bugfix

--- rag-cli/run_scraper.sh
+++ rag-cli/run_scraper.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 cd /universal_crawler
 echo "Running website crawl..."
-scrapy crawl universal --loglevel=INFO -a base=${SCRAPE_URL} -a subtree=1 -O "web_pages.jsonl" 
+scrapy crawl universal --loglevel=INFO -a base="$SCRAPE_URL" -a subtree=1 -O "web_pages.jsonl" 
 
 # Copy the results into the persisting docker volume
 cp web_pages.jsonl /data/web_pages.jsonl
